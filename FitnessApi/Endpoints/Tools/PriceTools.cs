f3b89d08ef307f715ec78c9ced042af04a4086c5
StoreMikkel <113053176+StoreMikkel@users.noreply.github.com>
2025-04-09 10:19:36 +0200

    first iteration, proof of concept testing

---
+++ FitnessApi/Endpoints/Tools/PriceTools.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.AI;
+
+namespace FitnessApi.Endpoints.Tools;
+
+public class PriceTools
+{
+    
+    [Description("Calculates the price of MIKKEL protein bars, returning a value in danish kroners.")]
+    public int CalculatePrice([Description("The number of MIKKEL protein bars to calculate a price for"), Required()]int count)
+    {
+        return count * 10;
+    }
+
+    public void Toast()
+    {
+        Console.WriteLine("!!!!Toast!!!!");
+        return;
+    }
+}
