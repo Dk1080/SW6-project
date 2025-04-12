80a3e31295b6729528ee56388f37020e84208d85
StoreMikkel <113053176+StoreMikkel@users.noreply.github.com>
2025-04-12 10:48:52 +0200

    rebasing

--- FitnessApi/Endpoints/Tools/DatabaseTools.cs
+++ FitnessApi/Endpoints/Tools/DatabaseTools.cs
@@ -5,10 +5,10 @@ namespace FitnessApi.Endpoints.Tools;
 
 public class DatabaseTools
 {
-    public string GetFitnessData(User user)
+    public string GetFitnessData(HealthInfo healthInfo)
     {
         //user.FitnessData assumed to be List<Dictionary<string,string>>
-        string output = JsonSerializer.Serialize(user.FitnessData);
+        string output = string.Join(", ", healthInfo.HourInfos);
         return output;
         //return "User has ran 10 kilometers every single day for the last week!";
     }
