58619e1e3402069557dfe93325acd1da7387406a
StoreMikkel <113053176+StoreMikkel@users.noreply.github.com>
2025-04-12 09:11:16 +0200

    inden rebase

---
+++ FitnessApi/Endpoints/Tools/DatabaseTools.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using FitnessApi.Models;
+
+namespace FitnessApi.Endpoints.Tools;
+
+public class DatabaseTools
+{
+    public string GetFitnessData(User user)
+    {
+        //user.FitnessData assumed to be List<Dictionary<string,string>>
+        string output = JsonSerializer.Serialize(user.FitnessData);
+        return output;
+        //return "User has ran 10 kilometers every single day for the last week!";
+    }
+}
