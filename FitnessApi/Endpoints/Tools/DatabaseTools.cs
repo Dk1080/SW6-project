05d284e24337f5d4a4dc1573cab2beeb8656a2cb
StoreMikkel <113053176+StoreMikkel@users.noreply.github.com>
2025-04-16 14:22:37 +0200

    added tool to set preferences and goals

--- FitnessApi/Endpoints/Tools/DatabaseTools.cs
+++ FitnessApi/Endpoints/Tools/DatabaseTools.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using FitnessApi.Models;
+using FitnessApi.Services;
 
 namespace FitnessApi.Endpoints.Tools;
 
@@ -10,5 +11,28 @@ public class DatabaseTools
     {
         string output = string.Join(", ", healthInfo.HourInfos);
         return output;
+    }
+
+    public async Task<string> SetPreferencesAndGoals(string username, string chartPreference, string goalType, string value, IUserPreferencesService userPreferencesService)
+    {
+        Console.WriteLine($"Preference and goals inputs: {username}, {chartPreference}, {goalType}, {value}");
+        UserPreferences preferences = new UserPreferences();
+        preferences.User = username;
+        preferences.ChartPreference = chartPreference;
+
+        List<Goal> goals = new List<Goal>();
+        goals.Add(new Goal() { GoalType = goalType, Value = int.Parse(value) });
+        preferences.Goals = goals;
+
+        try
+        {
+            var updatedPreferences = await userPreferencesService.UpdateUserPreferencesAsync(username, preferences);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return "Failure to update preferences";
+        }
+        return "Success, the users preferences and goals have been updated";
     }
 }
